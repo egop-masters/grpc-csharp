a31b385741fe842b1df9fb83d74170ecdc1b8eed
bird_egop <sampletext32@bk.ru>
2024-05-20 02:25:23 +0300

    switch to http

--- grpc-csharp/Program.cs
+++ grpc-csharp/Program.cs
@@ -46,8 +46,55 @@ app.MapGet(
                     await Task.Delay(5000);
                     iterations++;
                 }
+
                 await cts.CancelAsync();
-            });
+            }
+        );
+        logger.LogInformation("Launched grpc calling");
+        await Task.WhenAll(task1, task2);
+        return Results.Ok("Done");
+    }
+);
+
+app.MapGet(
+    "/http",
+    async ([FromServices] ILogger<Program> logger) =>
+    {
+        var cts = new CancellationTokenSource();
+        var task1 = Task.Run(
+            async () =>
+            {
+                var httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(
+                    builder.Configuration.GetConnectionString("HttpGoServiceUrl")
+                    ?? throw new InvalidOperationException("GrpcGoServiceUrl was not configured")
+                );
+
+                while (!cts.IsCancellationRequested)
+                {
+                    using var message = new HttpRequestMessage(HttpMethod.Get, "/")
+                    {
+                        Content = JsonContent.Create(new {Message = "Master"})
+                    };
+
+                    await httpClient.SendAsync(message);
+                }
+            }
+        );
+        var iterations = 0;
+        var task2 = Task.Run(
+            async () =>
+            {
+                while (iterations < 12)
+                {
+                    logger.LogInformation("Waiting already {time}s", iterations * 5);
+                    await Task.Delay(5000);
+                    iterations++;
+                }
+
+                await cts.CancelAsync();
+            }
+        );
         logger.LogInformation("Launched grpc calling");
         await Task.WhenAll(task1, task2);
         return Results.Ok("Done");
