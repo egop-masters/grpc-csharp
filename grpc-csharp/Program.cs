4599940192927e1f2caf557b447abf467db4fbad
bird_egop <sampletext32@bk.ru>
2024-05-20 02:05:50 +0300

    add grpc spamming

--- grpc-csharp/Program.cs
+++ grpc-csharp/Program.cs
@@ -14,16 +14,43 @@ builder.Services.AddGrpcClient<MasterService.MasterServiceClient>(
         );
     }
 );
+builder.Services.AddLogging(
+    x => x.ClearProviders()
+        .AddConsole()
+);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGet(
     "/",
-    async ([FromServices] MasterService.MasterServiceClient client) =>
+    async ([FromServices] MasterService.MasterServiceClient client, [FromServices] ILogger<Program> logger) =>
     {
-        var reply = await client.GetMasterDataAsync(new MasterRequest {Query = "World"});
-        return Results.Ok("Response: " + reply.Data);
+        var cts = new CancellationTokenSource();
+        var task1 = Task.Run(
+            async () =>
+            {
+                while (!cts.IsCancellationRequested)
+                {
+                    await client.GetMasterDataAsync(new MasterRequest {Query = "Master"});
+                }
+            }
+        );
+        var iterations = 0;
+        var task2 = Task.Run(
+            async () =>
+            {
+                while (iterations < 12)
+                {
+                    logger.LogInformation("Waiting already {time}s", iterations * 5);
+                    await Task.Delay(5000);
+                    iterations++;
+                }
+                await cts.CancelAsync();
+            });
+        logger.LogInformation("Launched grpc calling");
+        await Task.WhenAll(task1, task2);
+        return Results.Ok("Done");
     }
 );
 
